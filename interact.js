8848b79425151d60195d4772c8fecde3d7eb29cf
Andre Tres <andretres544@gmail.com>
2025-05-14 17:41:20 -0300

    Update interact.js

--- interact.js
+++ interact.js
@@ -12,3 +12,11 @@ const fadeElements = document.querySelectorAll('.fade-in');
   });
 
   fadeElements.forEach(el => observer.observe(el));
+
+  // Menu responsivo (hambúrguer)
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
